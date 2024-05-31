e08523b2adaef68f9c2e01b6c76d89825dc14d98
Yusuke Shinyama <yshinyama@vmware.com>
2024-05-31 16:36:06 +0900

    Changed: use TestRestTemplate instead of RestTemplate.

--- src/test/kotlin/com/example/todoApp/TodoAppApplicationTests.kt
+++ src/test/kotlin/com/example/todoApp/TodoAppApplicationTests.kt
@@ -5,17 +5,15 @@ import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.web.client.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TodoAppApplicationTests(@Autowired val builder: RestTemplateBuilder, @LocalServerPort val port: Int) {
-
-	final val restTemplate = builder.build()
+class TodoAppApplicationTests(@Autowired val restTemplate: TestRestTemplate, @LocalServerPort val port: Int) {
 
 	@Test
 	fun contextLoads() {
@@ -23,7 +21,7 @@ class TodoAppApplicationTests(@Autowired val builder: RestTemplateBuilder, @Loca
 
 	@Test
 	fun `GETリクエストはOKステータスを返す`() {
-		val response = restTemplate.getForEntity<Any>("http://localhost:$port/todos")
+		val response = restTemplate.getForEntity("http://localhost:$port/todos", Any::class.java)
 		assertThat(response.statusCode, equalTo(HttpStatus.OK))
 		assertThat(response.headers.contentType, equalTo(MediaType.APPLICATION_JSON))
 	}
@@ -31,7 +29,7 @@ class TodoAppApplicationTests(@Autowired val builder: RestTemplateBuilder, @Loca
 	@Test
 	@Sql("/insert_test_data.sql")
 	fun `GETリクエストはTodoオブジェクトのリストを返す`() {
-		val response = restTemplate.getForEntity<Array<Todo>>("http://localhost:$port/todos")
+		val response = restTemplate.getForEntity("http://localhost:$port/todos", Array<Todo>::class.java)
 		val todos = response.body!!
 		assertThat(todos.size, equalTo(1))
 		assertThat(todos[0].id, equalTo(1))
@@ -41,19 +39,19 @@ class TodoAppApplicationTests(@Autowired val builder: RestTemplateBuilder, @Loca
 	@Test
 	fun `POSTリクエストはOKステータスを返す`() {
 		val request = TodoRequest("hello")
-		val response = restTemplate.postForEntity<Any>("http://localhost:$port/todos", request)
+		val response = restTemplate.postForEntity("http://localhost:$port/todos", request, Any::class.java)
 		assertThat(response.statusCode, equalTo(HttpStatus.OK))
 	}
 
 	@Test
 	fun `POSTリクエストはTodoオブジェクトを格納する`() {
-		val response1 = restTemplate.getForEntity<Array<Todo>>("http://localhost:$port/todos")
+		val response1 = restTemplate.getForEntity("http://localhost:$port/todos", Array<Todo>::class.java)
 		val todos1 = response1.body!!
 
 		val request = TodoRequest("hello")
-		restTemplate.postForEntity<Any>("http://localhost:$port/todos", request)
+		restTemplate.postForEntity("http://localhost:$port/todos", request, Any::class.java)
 
-		val response2 = restTemplate.getForEntity<Array<Todo>>("http://localhost:$port/todos")
+		val response2 = restTemplate.getForEntity("http://localhost:$port/todos", Array<Todo>::class.java)
 		val todos2 = response2.body!!
 		assertThat(todos2.size, equalTo(todos1.size + 1))
 		assertThat(todos2.map { todo: Todo -> todo.text }, hasItem("hello"))
