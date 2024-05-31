6c3181504dcc02f5b3877ead5076be29cb9ad347
Yusuke Shinyama <yshinyama@vmware.com>
2024-05-31 15:20:08 +0900

    Changed: use RestTemplate instead of MockMvc.

--- src/test/kotlin/com/example/todoApp/TodoAppApplicationTests.kt
+++ src/test/kotlin/com/example/todoApp/TodoAppApplicationTests.kt
@@ -1,25 +1,21 @@
 package com.example.todoApp
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.hamcrest.Matchers.hasItem
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.web.client.*
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class TodoAppApplicationTests() {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TodoAppApplicationTests(@Autowired val builder: RestTemplateBuilder, @LocalServerPort val port: Int) {
 
-	@Autowired
-	lateinit var mockMvc: MockMvc
-	@Autowired
-	lateinit var objectMapper: ObjectMapper
+	final val restTemplate = builder.build()
 
 	@Test
 	fun contextLoads() {
@@ -27,42 +23,39 @@ class TodoAppApplicationTests() {
 
 	@Test
 	fun `GETリクエストはOKステータスを返す`() {
-		mockMvc.perform(get("/todos"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+		val response = restTemplate.getForEntity<Any>("http://localhost:$port/todos")
+		assertThat(response.statusCode, equalTo(HttpStatus.OK))
+		assertThat(response.headers.contentType, equalTo(MediaType.APPLICATION_JSON))
 	}
 
 	@Test
 	@Sql("/insert_test_data.sql")
 	fun `GETリクエストはTodoオブジェクトのリストを返す`() {
-		mockMvc.perform(get("/todos"))
-			.andExpect(jsonPath("$.length()").value(1))
-			.andExpect(jsonPath("$[0].id").value(1))
-			.andExpect(jsonPath("$[0].text").value("foo"))
+		val response = restTemplate.getForEntity<Array<Todo>>("http://localhost:$port/todos")
+		val todos = response.body!!
+		assertThat(todos.size, equalTo(1))
+		assertThat(todos[0].id, equalTo(1))
+		assertThat(todos[0].text, equalTo("foo"))
 	}
 
 	@Test
 	fun `POSTリクエストはOKステータスを返す`() {
-		val json = "{\"text\": \"hello\"}"
-		mockMvc.perform(post("/todos")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(json))
-			.andExpect(status().isOk())
+		val request = TodoRequest("hello")
+		val response = restTemplate.postForEntity<Any>("http://localhost:$port/todos", request)
+		assertThat(response.statusCode, equalTo(HttpStatus.OK))
 	}
 
 	@Test
 	fun `POSTリクエストはTodoオブジェクトを格納する`() {
-		val result = mockMvc.perform(get("/todos")).andReturn()
-		val json1 = result.response.contentAsString
-		val todos = objectMapper.readValue(json1, List::class.java)
-		val length = todos.count()
-		val json = "{\"text\": \"hello\"}"
-		mockMvc.perform(post("/todos")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(json))
-			.andReturn()
-		mockMvc.perform(get("/todos"))
-			.andExpect(jsonPath("$.length()").value(length+1))
-			.andExpect(jsonPath("$[*].text", hasItem("hello")))
+		val response1 = restTemplate.getForEntity<Array<Todo>>("http://localhost:$port/todos")
+		val todos1 = response1.body!!
+
+		val request = TodoRequest("hello")
+		restTemplate.postForEntity<Any>("http://localhost:$port/todos", request)
+
+		val response2 = restTemplate.getForEntity<Array<Todo>>("http://localhost:$port/todos")
+		val todos2 = response2.body!!
+		assertThat(todos2.size, equalTo(todos1.size + 1))
+		assertThat(todos2.map { todo: Todo -> todo.text }, hasItem("hello"))
 	}
 }
