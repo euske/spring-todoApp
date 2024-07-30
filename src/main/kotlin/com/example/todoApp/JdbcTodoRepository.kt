c187bced2cbdb7aa0915d3f1e4f491e22fe6464f
Yusuke Shinyama <yshinyama@vmware.com>
2024-07-30 17:16:53 +0900

    Renamed: TodoRepository -> JdbcTodoRepository.

---
+++ src/main/kotlin/com/example/todoApp/JdbcTodoRepository.kt
@@ -0,0 +1,47 @@
+package com.example.todoApp
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.simple.JdbcClient
+import org.springframework.jdbc.support.GeneratedKeyHolder
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
+import java.sql.ResultSet
+
+@Component
+class TodoRowMapper : RowMapper<Todo> {
+    override fun mapRow(rs: ResultSet, rowNum: Int): Todo {
+        return Todo(rs.getLong(1), rs.getString(2))
+    }
+}
+
+@Repository
+class JdbcTodoRepository (
+    @Autowired val jdbcClient: JdbcClient,
+    @Autowired val todoRowMapper: TodoRowMapper
+) : TodoRepository {
+
+    override fun getTodos(): List<Todo> {
+        return jdbcClient.sql("SELECT id, text FROM todos").query(todoRowMapper).list()
+    }
+
+    override fun getTodo(id: Long): Todo? {
+        val todos = jdbcClient.sql("SELECT id, text FROM todos WHERE id=:id").param("id", id).query(todoRowMapper).list()
+        if (todos.isEmpty()) {
+            return null
+        } else {
+            return todos[0]
+        }
+    }
+
+    override fun saveTodo(todoRequest: TodoRequest): Long {
+        val keyHolder = GeneratedKeyHolder()
+        jdbcClient.sql("INSERT INTO todos (text) VALUES (:text)").param("text", todoRequest.text).update(keyHolder, "id")
+        val newId = keyHolder.key!!.toLong()
+        return newId
+    }
+
+    override fun deleteTodo(id: Long) {
+        jdbcClient.sql("DELETE FROM todos WHERE id=:id").param("id", id).update()
+    }
+}
